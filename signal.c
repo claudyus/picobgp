783d9c54982856cc61ef3c5be73dc5d9b2a05f6d
Claudio Mignanti <c.mignanti@gmail.com>
2012-01-13 15:46:51 +0100

    iter over the spammed udp msgs

--- signal.c
+++ signal.c
@@ -8,9 +8,9 @@ void let_me_spam(){
 	raddr.sin_addr.s_addr = inet_addr("255.255.255.255");
 	raddr.sin_port = htons(9876);
 
-	for (i=0; i < ipslen; i++)
+	for (i=0; i < ipslen; i++) {
 		
-		if (sendto(sd, &(ips[i]), sizeof(bgpmsg), 0, &raddr, )==-1)
+		if (sendto(sd, &(ips[i]), sizeof(bgpmsg), 0, &raddr, sizeof(struct sockaddr_in *)) == -1) {
 			perror("sendto");
 			exit(1);
 		}
