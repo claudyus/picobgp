8c3b825e48c8acacab6a9ec7fdd3ec72d2b5c08b
Claudio Mignanti <c.mignanti@gmail.com>
2012-01-13 15:52:53 +0100

    bind of route socket and fix on udp socket

--- main.c
+++ main.c
@@ -10,21 +10,30 @@ int main (int argc, char *argv[]) {
 	sigaction(SIGUSR1, &handler, 0);
 	sigaction(SIGALRM, &handler, 0);
 
+	/* udp socket */
 	sd = socket(AF_INET, SOCK_DGRAM, 0);
-
 	DEFandNULL(struct sockaddr_in, server_sock);
 	server_sock.sin_family = AF_INET;
 	server_sock.sin_port = htons(9876);
 
 	/*retrieve address from device */
 	strncpy(ifr.ifr_name, "eth0", IFNAMSIZ-1);	//FIXME getopt da ste parti
-	ioctl(fd, SIOCGIFADDR, &server_sock);
+	ioctl(sd, SIOCGIFADDR, &server_sock);
 
 	if ( bind(sd, (struct sockaddr*) &server_sock, sizeof(server_sock)) < 0 ) {
 		perror("bind");
 		return 1;
 	}
 
+	/* bind the route socket */
+	rd = socket(AF_INET, SOCK_RAW, NETLINK_ROUTE);
+	DEFandNULL(struct sockaddr_nl, la);
+	la.nl_family = AF_NETLINK;
+	la.pid = getpid();
+	if (bind(rd, &la, sizeof(la)) == -1) {
+		perror ("route bind");
+		return 1;
+	}
 
 	alarm(3);	//start the spamming process
 
