906586dd42a4dad50930e2695e8b894d479fd8a4
Claudio Mignanti <c.mignanti@gmail.com>
2012-01-17 09:27:32 +0100

    fix route placement

--- route.c
+++ route.c
@@ -1,15 +1,14 @@
 
 #include "pbgp.h"
 
-/* see http://www.cs.clemson.edu/~westall/826/zebra-0.92a/zebra/rt_ioctl.c for route table manipolation*/
 void clean_rt(void) {
 	int i;
 	for (i = 1; i <= ann_len; i++) {
 		if (difftime(time(NULL), ann_ips[i].l_update) > 10) {
 			/* network not more reachable */
 			DEFandNULL(struct rtentry, rtentry)
-			((struct sockaddr_in *)&rtentry.rt_dst)->sin_family = AF_INET;
-			((struct sockaddr_in *)&rtentry.rt_gateway)->sin_family = AF_INET;
+			((struct sockaddr_in *)&rtentry.rt_dst)->sin_family = \
+			((struct sockaddr_in *)&rtentry.rt_gateway)->sin_family = \
 			((struct sockaddr_in *)&rtentry.rt_genmask)->sin_family = AF_INET;
 			((struct sockaddr_in *)&rtentry.rt_dst)->sin_addr.s_addr = ann_ips[i].addr.s_addr;
 			((struct sockaddr_in *)&rtentry.rt_genmask)->sin_addr.s_addr = ann_ips[i].netmask.s_addr;
@@ -33,24 +32,22 @@ int update_rt (bgpmsg flood_msg, int len) {
 		if (memcmp(&ann_ips[i].addr, &(flood_msg.addr), sizeof(struct in_addr)*3) == 0)
 			printf ("update_rt: announce already in ann_ips\n");
 			time(&ann_ips[i].l_update);
-			return 0;	//route already in place
+			return 0;	/* route already in place */
 	}
 
 	DEFandNULL(struct rtentry, rtentry);
-	((struct sockaddr_in *)&rtentry.rt_dst)->sin_family = AF_INET;
-	((struct sockaddr_in *)&rtentry.rt_gateway)->sin_family = AF_INET;
+	((struct sockaddr_in *)&rtentry.rt_dst)->sin_family = \
+	((struct sockaddr_in *)&rtentry.rt_gateway)->sin_family = \
 	((struct sockaddr_in *)&rtentry.rt_genmask)->sin_family = AF_INET;
 
 	((struct sockaddr_in *)&rtentry.rt_dst)->sin_addr.s_addr = flood_msg.addr.s_addr;
+	((struct sockaddr_in *)&rtentry.rt_gateway)->sin_addr.s_addr = 0;
 	((struct sockaddr_in *)&rtentry.rt_genmask)->sin_addr.s_addr = flood_msg.netmask.s_addr;
-	((struct sockaddr_in *)&rtentry.rt_gateway)->sin_addr.s_addr = flood_msg.loc_addr.s_addr;
-	//ftp://ftp.gwdg.de/linux/funet/PEOPLE/.Linus.old/route.c
-	//strcpy(&rtentry.rt_if, local_ifname);
-	rtentry.rt_flags = RTF_UP | RTF_GATEWAY;
-	rtentry.rt_metric = 2;
-	rtentry.rt_mtu = 1;
 
-	fprintf(stderr, "received %s netmask %s\n", inet_ntoa(flood_msg.addr), inet_ntoa(flood_msg.netmask));
+	rtentry.rt_dev = local_ifname
+	rtentry.rt_flags = RTF_UP;
+
+	//fprintf(stderr, "received %s netmask %d, localaddr: %d\n", inet_ntoa(flood_msg.addr), flood_msg.netmask, flood_msg.loc_addr);
 
 	if(ioctl (sd, SIOCADDRT, &rtentry) == -1) {
 		perror("ioctl(SIOCADDRT) are you root?");
